939b0e818fd579f4cf5d3d80cf1f78ea24f1ab06
ChebotarevaMR <chebotareva.m.r@yandex.ru>
2020-12-11 16:34:09 +0300

    Added Player and animation

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Animator Animator;
+    public float Speed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Animator.SetBool("isWalkUp", true);
+            transform.position += Vector3.up * Speed * Time.deltaTime;
+        }
+        else
+        {
+            Animator.SetBool("isWalkUp", false);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Animator.SetBool("isWalkDown", true);
+            transform.position -= Vector3.up * Speed * Time.deltaTime;
+        }
+        else
+        {
+            Animator.SetBool("isWalkDown", false);
+        }
+    }
+}
