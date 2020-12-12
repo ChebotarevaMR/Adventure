dc3733e85f2c83d7a428f22d221f4806b5793f32
ChebotarevaMR <chebotareva.m.r@yandex.ru>
2020-12-12 13:33:55 +0300

    Fixed animation

---
+++ Assets/Scripts/PictureAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PictureAnimation : MonoBehaviour
+{
+    public Animator Animator;
+    public float Delay = 10;
+    private float _currentTime;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_currentTime > Delay)
+        {
+            _currentTime = 0;
+            var value = Animator.GetBool("State");
+            Animator.SetBool("State", !value);
+        }
+        _currentTime += Time.deltaTime;
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,46 +5,77 @@ public class PlayerMove : MonoBehaviour
     public enum State { Idle, WalkUp,WalkDown, WalkRight, WalkLeft};
 
     public Animator Animator;
-    public float Speed = 1.0f;
+    public float Speed = 10.0f;
+    public Collider2D Collider;
     private State _state = State.Idle;
     private Vector3 _dir;
-    private Rigidbody2D _rigidBody;
+    private Collider2D _collider;
+    private int _key = 1;
+
     private void Start()
     {
-        _rigidBody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<BoxCollider2D>();
     }
     private void FixedUpdate()
     {
-        if (_state != State.Idle)
-        {
-            _rigidBody.MovePosition(transform.position + _dir * Speed * Time.deltaTime);
-            //transform.position += _dir * Speed * Time.deltaTime;
-        }
+        
     }
+
+    private bool OverlapExist()
+    {
+
+        Collider2D[] allOverlappingColliders = new Collider2D[16];
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+
+        int overlapCount = Physics2D.OverlapCollider(Collider, contactFilter, allOverlappingColliders);
+        string result = "";
+        int ignore = 0;
+        if (overlapCount > 0)
+        {
+            for (int i = 0; i < overlapCount; i++)
+            {
+                Collider2D x = allOverlappingColliders[i];
+                if (x.gameObject.GetComponent<IgnoreCollider>() != null || !x.bounds.Intersects(Collider.bounds)) ignore++;
+                result += " " + x.gameObject.name + " " + x.bounds.Intersects(Collider.bounds) + " " + x.bounds + " " + Collider.bounds;
+            }
+        }
+        return (overlapCount - ignore) != 0;
+    }
+
+
+
     // Update is called once per frame
     private void Update()
     {
         var currentState = State.Idle;
         _dir = new Vector3(0, 0, 0);
+        var key = _key;
         if (Input.GetKey(KeyCode.W))
         {
             currentState = State.WalkUp;
             _dir = Vector3.up;
+            key = 1;
         }
         if (Input.GetKey(KeyCode.S))
         {
             currentState = State.WalkDown;
             _dir = Vector3.down;
+            key = 2;
         }
         if (Input.GetKey(KeyCode.D))
         {
             currentState = State.WalkRight;
             _dir = Vector3.right;
+            key = 3;
         }
         if (Input.GetKey(KeyCode.A))
         {
             currentState = State.WalkLeft;
             _dir = Vector3.left;
+            key = 4;
         }
         // Action
         if (Input.GetKey(KeyCode.E))
@@ -58,10 +89,26 @@ public class PlayerMove : MonoBehaviour
         }
         if (_state != currentState)
         {
-            Debug.Log((int)currentState);
             Animator.SetInteger("State", (int)currentState);
             _state = currentState;
         }
-        
+        if(_key != key)
+        {
+            Animator.SetInteger("Key", key);
+            _key = key;
+        }
+        if (_state != State.Idle)
+        {
+            var shift = _dir * Speed * Time.deltaTime;
+            var position = transform.position + shift;
+            Collider.offset = _collider.offset + (Vector2)shift;
+
+            if (!OverlapExist())
+            {
+                transform.position = position;
+            }
+            Collider.offset = _collider.offset;
+        }
+
     }
 }
